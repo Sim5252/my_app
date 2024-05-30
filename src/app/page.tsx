01de3c3827b4c87e58a1e60cd500cc39d048e4ae
Sim5252 <qnpfr890@gmail.com>
2024-05-30 09:21:56 +0900

    (commit message)

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Component } from "@/components/component/component";
+import { Layout } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Layout />
+  );
+}
